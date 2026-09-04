dedaec859e59bab1ce47b45a4e0d42bcc34b3fdd
samir4325 <samir4325@github.com>
2026-09-04 23:19:51 +0530

    Fix vercel.json schema and add static dist build script

---
+++ build.js
@@ -0,0 +1,16 @@
+﻿const fs = require('fs');
+const path = require('path');
+
+const dist = path.join(__dirname, 'dist');
+if (!fs.existsSync(dist)) {
+  fs.mkdirSync(dist, { recursive: true });
+}
+
+fs.copyFileSync(path.join(__dirname, 'index.html'), path.join(dist, 'index.html'));
+
+const redirectsPath = path.join(__dirname, '_redirects');
+if (fs.existsSync(redirectsPath)) {
+  fs.copyFileSync(redirectsPath, path.join(dist, '_redirects'));
+}
+
+console.log('Build complete: copied index.html to dist/');
